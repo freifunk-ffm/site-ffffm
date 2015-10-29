d84a9f8e960a308c42815e96efdfb56b5938c780
JuS <sagurna@sagutech.de>
2015-10-29 16:31:36 +0100

    Anpassungen an Struktur von site.conf und site.mk für Gluon v2015.2 Release

--- site.mk
+++ site.mk
@@ -3,6 +3,8 @@ GLUON_SITE_PACKAGES := \
 	gluon-alfred \
 	gluon-announced \
 	gluon-autoupdater \
+	gluon-setup-mode \
+	gluon-config-mode-core \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
@@ -14,27 +16,49 @@ GLUON_SITE_PACKAGES := \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
 	gluon-luci-private-wifi \
+	gluon-luci-mesh-vpn-fastd \
+	gluon-luci-wifi-config \
+	gluon-luci-node-role \
+	gluon-luci-switchconfig \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
-	gluon-luci-switchconfig \
-	gluon-ffmautokey \
 	gluon-radvd \
 	gluon-status-page \
-	collectd5 \
-	collectd-mod-ping \
-	collectd-mod-interface \
-	collectd-mod-load \
-	collectd-mod-cpu \
-	collectd-mod-memory \
-	collectd-mod-network \
-	collectd-mod-iwinfo \
 	iwinfo \
 	iptables \
-	gluon-ffmcollectdconfig \
-	haveged
+	ffffm-ebtables-net-rules \
+	haveged 
 
 
-DEFAULT_GLUON_RELEASE := 0.4.2+0-exp$(shell date '+%Y%m%d')
+#####################################################################################################################
+# Firmware Versioning
+#####################################################################################################################
+# Release Stable: x.y-stable
+# Release Test:   x.y.z-test
+# Release Dev:    x.y.z.w-dev (+Date/Time)
+#
+# Geht eine Dev-Version in Test über, so können identische  Dateien in 'test' und 'dev' abgelegt werden.
+# Geht eine Test-Version in Stable über, so können identische Dateien in 'stable', 'test' und 'dev' abgelegt werden. 
+#
+# Autoupdate ist weiterhin gewehrleistet! 
+# Per Definition ist 'x.y-stable' grösser als 'x.y.z-test' und grösser als 'x.y.z.w-dev'
+#####################################################################################################################
+
+# This is the test branch -> x.y.z-test
+DEFAULT_GLUON_RELEASE := ffm-1.10.1-test        # This needs to be changed after each online release
+
+# For homebrew development add e.g. date and time 
+# (don't use the ':' char. It will break the build)
+# DEFAULT_GLUON_RELEASE := $(DEFAULT_GLUON_RELEASE)-$(shell date '+%m.%d-%H%M')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+# Development branch information
+GLUON_BRANCH ?= test
+
+# Default priority for updates.
+# GLUON_PRIORITY ?= 0
+
+# Languages to include
+GLUON_LANGS ?= en de
