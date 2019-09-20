1fa585fb67ef928d5bd1ee94b6965f5412fad68e
oszilloskop <>
2019-09-20 18:26:09 +0200

    Harte Übernahme der Test-Site

--- patches/README.md
+++ patches/README.md
@@ -1,33 +1,15 @@
 ## Lokale Site-Patches
 
 ### Anwendung
-Die Patch-Anwendungsaufrufe müssen direkt im Gluon-Ordner durchgeführt werden.
+Die hier enthaltenen Patches sind so gestaltet, dass die Patch-Anwendungsaufrufe direkt im Gluon-Build-Ordner durchgeführt werden müssen.
 
-Die Patches müssen im Build-Ablauf vor dem Befehl
+Wenn nicht anders angegeben, so müssen die Patches im Build-Ablauf meist vor dem Befehl
 
     gluon update
 angewendet werden.
 
 
-### Geübte Git-User
-Geübte Git-User können die hier enthaltenen Patches einzeln mittels der folgenden Befehlsfolge anwenden: 
+### Durchführung 
+Die hier enthaltenen Patches werden einzeln mittels der folgenden Befehlszeile direkt im Gluon-Build-Ordner angwendet:
 
-    git -c user.name='Homebrew Bro' -c user.email='HomebrewBro@void.example.com' -c commit.gpgsign=false am --whitespace=nowarn --committer-date-is-author-date ./site/patches/NameDesPatches.patch
-
-Mittels der folgenden Befehlsfolge werden alle Patches in einem Rutsch angewendet:
- 
-    git -c user.name='Homebrew Bro' -c user.email='HomebrewBro@void.example.com' -c commit.gpgsign=false am --whitespace=nowarn --committer-date-is-author-date ./site/patches/*.patch
-
-Falls es zu Problemen kommt, kann folgende Befehlszeile recht hilfreich sein:
-
-    git -c user.name='Homebrew Bro' -c user.email='HomebrewBro@void.example.com' -c commit.gpgsign=false am --abort
-
-Wenn nichts mehr geht, dann hilft:
-
-    git reset --hard origin/GluonBranchName
-    
-
-### Ungeübte Git-User 
-Ungeübte Git-User können die hier enthaltenen Patches einzeln mittels der folgenden Befehlszeile anwenden:
-
-    patch -N -p1 -i ./site/patches/NameDesPatches.patch 
+    patch -N -p1 -r - -i ./site/patches/NameDesPatches.patch
--- site.mk
+++ site.mk
@@ -13,9 +13,7 @@ GLUON_FEATURES := \
 	web-advanced \
 	web-wizard \
 	web-private-wifi \
-	web-logging
-
-#	config-mode-domain-select \
+	web-logging \
 
 
 GLUON_SITE_PACKAGES := \
@@ -24,43 +22,77 @@ GLUON_SITE_PACKAGES := \
 	haveged \
 	ffffm-banner_legacy \
 	ffffm-keep-radio-channel \
-	ffffm-autoupdater-use-site-conf-branch \
 	ffffm-button-bind \
 	ffffm-ath9k-broken-wifi-workaround \
 	gluon-ssid-changer \
+	ffffm-autoupdater-use-site-conf-branch \
 	-gluon-web-autoupdater \
 	respondd-module-airtime
 
-include $(GLUON_SITEDIR)/specific_site.mk 
+include $(GLUON_SITEDIR)/specific_site.mk
 
-# Multidomain support
-GLUON_MULTIDOMAIN=1
+# Enable multidomain support
+GLUON_MULTIDOMAIN := 1
 
-#####################################################################################################################
 
-# This is the Dev branch
+# This is the 'Test' site branch.
 
-# Gluon Base Release
-DEFAULT_GLUON_RELEASE := vHomebrew-dev
+########################################################################
+# BEGIN: "Generic homebrew build preparation"
+#
+# To build an own Frankfurt firmware please see:
+# https://github.com/freifunk-ffm/site-ffffm#eine-eigene-frankfurter-firmware-bauen
+########################################################################
 
-# For homebrew development add e.g. date and time 
+# Homebrew Gluon firmware release name
+HOMEBREW_GLUON_RELEASE := vHomebrew-test
+
+# For homebrew development add e.g. date and time
 # (Note: Don't use the ':' char. It will break the build)
-DEFAULT_GLUON_RELEASE := $(DEFAULT_GLUON_RELEASE)-$(shell date '+%Y.%m.%d')
+HOMEBREW_GLUON_RELEASE := $(HOMEBREW_GLUON_RELEASE)-$(shell date '+%m%d')
 
-# Allow overriding the release number from the command line
-GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+########################################################################
+# END: "Generic homebrew build preparation"
+#
+# Admin/Support hint:
+# With the few lines above you can identify exactly which branch of our
+# site was used to create the homebrew firmware.
+########################################################################
 
-# Development branch information
-GLUON_BRANCH ?= dev
+###
+
+########################################################################
+# Normally the important parameters are specified by the superimposed
+# build process (e.g. CI and/or CD).
+# If this is not the case the following '?=' assignments apply.
+########################################################################
+
+# Gluon release information.
+# Allow overriding from the command line.
+GLUON_RELEASE ?= $(HOMEBREW_GLUON_RELEASE)
+
+# Autoupdater default branch.
+# Allow overriding from the command line.
+GLUON_BRANCH ?= test
 
 # Default priority for updates.
+# Allow overriding from the command line.
 GLUON_PRIORITY ?= 0
 
-# Languages to include
+# Languages to include.
+# Allow overriding from the command line.
 GLUON_LANGS ?= de
 
-# region information for regulatory compliance
+# Region information for regulatory compliance.
+# Allow overriding from the command line.
 GLUON_REGION ?= eu
 
-# Prefer ath10k firmware with given mesh support (ibss or 11s)
+# Prefer ath10k firmware with given mesh support (ibss or 11s).
+# Allow overriding from the command line.
 GLUON_WLAN_MESH ?= 11s
+
+# Build of image types.
+# Allow overriding from the command line.
+GLUON_DEPRECATED ?= full
+
+###
